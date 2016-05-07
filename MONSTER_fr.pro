ceab4c0cd79b2d829a040b3ca5b5277a12a1c50b
Matteo Martincigh <m.martincigh@gmail.com>
2016-05-07 11:08:02 +0100

    Improved arguments parsing: now it is possible to process both directories and files simultaneously. Update version to 0.3.0.

--- MONSTER_fr.pro
+++ MONSTER_fr.pro
@@ -2,7 +2,7 @@ TARGET = MONSTER_fr
 
 TEMPLATE = app
 
-VERSION = 0.2.4
+VERSION = 0.3.0
 
 QT += \
     core \
--- main.cpp
+++ main.cpp
@@ -23,6 +23,180 @@ static const QString FLIPBOARD_FILTER           ("^[0-9a-fA-F]{40}\\.(?i)(jpe?g|
 static const QString GOOGLE_IMAGES_FILTER       ("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\\.(?i)(jpe?g|png|gif|bmp)$");
 static const QString ANDROID_FILTER             ("^IMG_[0-9]{8}_[0-9]{6}\\.(?i)(jpe?g|png|gif|bmp)$");
 
+static QStringList FILE_FILTERS;
+
+int parseArguments(const QStringList &arguments, QList<QDir> &directories, QFileInfoList &files)
+{
+    // Check whether the argument list is empty.
+    if (arguments.empty())
+    {
+        qWarning() << "WARNING> Empty argument list";
+
+        return -1;
+    }
+
+    // Process arguments (skip the executable entry).
+    for (int i = 1; i < arguments.count(); i++)
+    {
+        QString argument(arguments.at(i));
+
+        qDebug() << "Processing argument:" << qPrintable(argument);
+
+        // Check whether the argument is a directory or a file (or neither).
+        QFileInfo argument_file_info(argument);
+        if (!argument_file_info.exists())
+        {
+            // Skip to the next argument.
+
+            continue;
+        }
+
+        // Check whether the argument is a directory.
+        if (argument_file_info.isDir())
+        {
+            // Append the argument to the list of directories.
+            directories.append(QDir(argument));
+        }
+
+        // Check whether the argument is a file.
+        else if (argument_file_info.isFile())
+        {
+            // Append the argument to the list of files.
+            files.append(argument_file_info);
+        }
+    }
+
+    return 0;
+}
+
+int renameFile(const QFileInfo &file)
+{
+    QString file_name = file.fileName();
+
+    qDebug() << "Current file:" << qPrintable(file_name);
+
+    // Check if the current file needs to be renamed.
+    bool file_filter_match = false;
+    foreach (const QString &image_filter, FILE_FILTERS)
+    {
+        QRegularExpression regular_expression(image_filter);
+        QRegularExpressionMatch regular_expression_match = regular_expression.match(file_name);
+        if (regular_expression_match.hasMatch())
+        {
+            file_filter_match = true;
+
+            continue;
+        }
+    }
+    if (!file_filter_match)
+    {
+        qWarning() << "WARNING> File" << file_name << "doesn't match any of the filters, skipping...";
+
+        return -1;
+    }
+
+    QString file_absolute_path = file.absoluteFilePath();
+    QString exif_data_image_timestamp;
+    try
+    {
+        std::string image_absolute_path_string = file_absolute_path.toStdString();
+        Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(image_absolute_path_string);
+        if (image.get() == NULL)
+        {
+            qWarning() << "WARNING> Cannot load image:" << qPrintable(file_name);
+
+            return -2;
+        }
+
+        image->readMetadata();
+        Exiv2::ExifData &exif_data = image->exifData();
+        Exiv2::ExifKey exif_key(IMAGE_TIMESTAMP_TAG.toStdString());
+        Exiv2::ExifData::const_iterator pos = exif_data.findKey(exif_key);
+        if (pos != exif_data.end())
+        {
+            QString exif_data_value = QString::fromStdString(pos->toString());
+            QStringList exif_data_image_timestamp_date_time_split = exif_data_value.split(' ');
+            if (exif_data_image_timestamp_date_time_split.size() < 2)
+            {
+                qWarning() << "WARNING> Invalid image timestamp";
+
+                return -3;
+            }
+
+            QString exif_data_image_timestamp_date = exif_data_image_timestamp_date_time_split.at(0);
+            exif_data_image_timestamp_date.replace(':', '-');
+            QString exif_data_image_timestamp_time = exif_data_image_timestamp_date_time_split.at(1);
+            exif_data_image_timestamp_time.replace(':', '.');
+            exif_data_image_timestamp = exif_data_image_timestamp_date + " " + exif_data_image_timestamp_time;
+        }
+        else
+        {
+            qDebug() << "No image timestamp, using file attributes...";
+
+            QDateTime image_file_last_modified_time = file.lastModified();
+            exif_data_image_timestamp = image_file_last_modified_time.toString("yyyy-MM-dd HH.mm.ss");
+        }
+    }
+    catch (Exiv2::AnyError &e)
+    {
+        qCritical() << "ERROR> Caught Exiv2 exception:" << e.what();
+
+        return -4;
+    }
+
+    qDebug() << "Image timestamp:" << qPrintable(exif_data_image_timestamp);
+
+    QString new_image_name = exif_data_image_timestamp + "." + file.completeSuffix();
+
+    qDebug() << "New image name:" << qPrintable(new_image_name);
+
+    QDir file_absolute_directory(file.absoluteDir());
+    QString new_image_file_name = file_absolute_directory.filePath(new_image_name);
+    QFileInfo new_image_file_info(new_image_file_name);
+    if (new_image_file_info.exists())
+    {
+        qWarning() << "WARNING> File" << new_image_name << "already exists, skipping...";
+
+        return -5;
+    }
+
+    // Rename the file.
+    QFile new_image_file(file_absolute_path);
+    bool ret_val = new_image_file.rename(new_image_file_name);
+    if (!ret_val)
+    {
+        qWarning() << "WARNING> Cannot rename file to:" << qPrintable(new_image_name);
+
+        return -6;
+    }
+
+    qDebug() << "File renamed to:" << qPrintable(new_image_file_info.absoluteFilePath());
+
+    return 0;
+}
+
+int renameFiles(const QDir &directory)
+{
+    QFileInfoList files = directory.entryInfoList(QDir::Files, QDir::Name);
+    //int image_files_count = image_files.count();
+    //int images_renamed = 0;
+
+    foreach (const QFileInfo &file, files)
+    {
+        qDebug() << "----------------";
+
+        int ret_val = renameFile(file);
+        if (ret_val < 0)
+        {
+            qWarning() << "WARNING> Cannot rename file:" << qPrintable(file.fileName());
+        }
+
+        //qDebug().nospace() << ++images_renamed << "/" << image_files_count;
+    }
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication application(argc, argv);
@@ -31,19 +205,29 @@ int main(int argc, char *argv[])
 
     qDebug().nospace() << "MONSTER_fr v" << qPrintable(application.applicationVersion());
 
+    // Prepare file filters;
+    FILE_FILTERS << TUMBLR_FILTER_1
+                 << TUMBLR_FILTER_2
+                 << TUMBLR_FILTER_3
+                 << PHONEGRAM_FILTER
+                 << TELEGRAM_FILTER
+                 << RUNKEEPER_APP_FILTER
+                 << RUNKEEPER_WEB_FILTER
+                 << FLIPBOARD_FILTER
+                 << GOOGLE_IMAGES_FILTER
+                 << ANDROID_FILTER;
+
     QDir working_directory(DEFAULT_WORKING_DIRECTORY);
 
-    if (application.arguments().count() > 1)
+    // Parse arguments.
+    QList<QDir> directories;
+    QFileInfoList files;
+    int ret_val = parseArguments(application.arguments(), directories, files);
+    if (ret_val < 0)
     {
-        qDebug() << "Processing argument:" << qPrintable(application.arguments().at(1));
+        qCritical() << "ERROR> Error parsing arguments";
 
-        working_directory = application.arguments().at(1);
-        if (!working_directory.exists())
-        {
-            qWarning() << "WARNING> Passed working directory" << working_directory.path() << "does not exist";
-
-            return EXIT_FAILURE;
-        }
+        return EXIT_FAILURE;
     }
 
     qDebug() << "Working directory:" << qPrintable(working_directory.path());
@@ -55,132 +239,31 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    QFileInfoList image_files = working_directory.entryInfoList(QDir::Files, QDir::Name);
-    int image_files_count = image_files.count();
-    int images_renamed = 0;
+    // Process directories.
+    foreach (const QDir &directory, directories)
+    {
+        qDebug() << "================";
 
-    // Build image filter list.
-    QStringList image_filters;
-    image_filters << TUMBLR_FILTER_1
-                  << TUMBLR_FILTER_2
-                  << TUMBLR_FILTER_3
-                  << PHONEGRAM_FILTER
-                  << TELEGRAM_FILTER
-                  << RUNKEEPER_APP_FILTER
-                  << RUNKEEPER_WEB_FILTER
-                  << FLIPBOARD_FILTER
-                  << GOOGLE_IMAGES_FILTER
-                  << ANDROID_FILTER;
+        ret_val = renameFiles(directory);
+        if (ret_val < 0)
+        {
+            qWarning() << "WARNING> Cannot rename files in directory:" << qPrintable(directory.path());
+        }
+    }
 
-    foreach (const QFileInfo &image_file, image_files)
+    // Process files.
+    foreach (const QFileInfo &file, files)
     {
         qDebug() << "----------------";
 
-        QString image_name = image_file.fileName();
-        QString image_absolute_path = image_file.absoluteFilePath();
-        QString exif_data_image_timestamp;
-
-        qDebug() << "Current image:" << qPrintable(image_name);
-
-        // Check if the current image needs to be renamed.
-        bool image_filter_match = false;
-        foreach (const QString &image_filter, image_filters)
+        ret_val = renameFile(file);
+        if (ret_val < 0)
         {
-            QRegularExpression regular_expression(image_filter);
-            QRegularExpressionMatch regular_expression_match = regular_expression.match(image_name);
-            if (regular_expression_match.hasMatch())
-            {
-                image_filter_match = true;
-
-                continue;
-            }
+            qWarning() << "WARNING> Cannot rename file:" << qPrintable(file.fileName());
         }
-
-        if (!image_filter_match)
-        {
-            qWarning() << "WARNING> File" << image_name << "doesn't match any of the filters, skipping...";
-
-            continue;
-        }
-
-        try
-        {
-            std::string image_absolute_path_string = image_absolute_path.toStdString();
-            Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(image_absolute_path_string);
-            if (image.get() == NULL)
-            {
-                qWarning() << "WARNING> Cannot load image:" << qPrintable(image_name);
-
-                continue;
-            }
-
-            image->readMetadata();
-            Exiv2::ExifData &exif_data = image->exifData();
-            Exiv2::ExifKey exif_key(IMAGE_TIMESTAMP_TAG.toStdString());
-            Exiv2::ExifData::const_iterator pos = exif_data.findKey(exif_key);
-            if (pos != exif_data.end())
-            {
-                QString exif_data_value = QString::fromStdString(pos->toString());
-                QStringList exif_data_image_timestamp_date_time_split = exif_data_value.split(' ');
-                if (exif_data_image_timestamp_date_time_split.size() < 2)
-                {
-                    qWarning() << "WARNING> Invalid image timestamp";
-
-                    continue;
-                }
-
-                QString exif_data_image_timestamp_date = exif_data_image_timestamp_date_time_split.at(0);
-                exif_data_image_timestamp_date.replace(':', '-');
-                QString exif_data_image_timestamp_time = exif_data_image_timestamp_date_time_split.at(1);
-                exif_data_image_timestamp_time.replace(':', '.');
-                exif_data_image_timestamp = exif_data_image_timestamp_date + " " + exif_data_image_timestamp_time;
-            }
-            else
-            {
-                qDebug() << "No image timestamp, using file attributes...";
-
-                QDateTime image_file_last_modified_time = image_file.lastModified();
-                exif_data_image_timestamp = image_file_last_modified_time.toString("yyyy-MM-dd HH.mm.ss");
-            }
-        }
-        catch (Exiv2::AnyError &e)
-        {
-            qCritical() << "ERROR> Caught Exiv2 exception:" << e.what();
-
-            return EXIT_FAILURE;
-        }
-
-        qDebug() << "Image timestamp:" << qPrintable(exif_data_image_timestamp);
-
-        QString new_image_name = exif_data_image_timestamp + "." + image_file.completeSuffix();
-
-        qDebug() << "New image name:" << qPrintable(new_image_name);
-
-        QString new_image_file_name = working_directory.filePath(new_image_name);
-        QFileInfo new_image_file_info(new_image_file_name);
-        if (new_image_file_info.exists())
-        {
-            qWarning() << "WARNING> File" << new_image_name << "already exists, skipping...";
-
-            continue;
-        }
-
-        // Rename the file.
-        QFile new_image_file(image_absolute_path);
-        bool ret_val = new_image_file.rename(working_directory.filePath(new_image_name));
-        if (!ret_val)
-        {
-            qWarning() << "WARNING> Cannot rename file to:" << qPrintable(new_image_name);
-
-            continue;
-        }
-
-        qDebug() << "File renamed to:" << qPrintable(new_image_file_info.absoluteFilePath());
-
-        qDebug().nospace() << ++images_renamed << "/" << image_files_count;
     }
 
-    qDebug() << "----------------";
+    qDebug() << "================";
 
     qDebug() << "Done";
 
